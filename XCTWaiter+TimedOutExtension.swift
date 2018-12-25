014079b4ef1c648093550de26e47df166b6158f5
Aleksandr Pavliuk <pavliuk.aleksandr@gmail.com>
2018-12-25 18:46:33 +0200

    Added swift file with extension

---
+++ XCTWaiter+TimedOutExtension.swift
@@ -0,0 +1,16 @@
+//
+//  Created by Aleksandr Pavliuk on 12/25/18.
+//
+
+import Foundation
+import XCTest
+
+extension XCTWaiter {
+    static func waitForExpectation(withDescription: String,
+                                   timedOut seconds: TimeInterval,
+                                   andVerify assetsBlock:(() -> Void)) {
+        let expectation = XCTestExpectation(description: withDescription)
+        let _ = XCTWaiter.wait(for: [expectation], timeout: seconds)
+        assetsBlock()
+    }
+}
